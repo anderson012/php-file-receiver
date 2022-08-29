a1618d825bf1aabf78e517d162ade2d908b452c0
anderson012 <andersoncarlos28@gmail.com>
2022-08-29 16:18:22 -0400

    chore: orthograph

--- controllers/response.controller.php
+++ controllers/response.controller.php
@@ -8,7 +8,7 @@
         public static function makeResponse($msg,  $code = ResponseStatus::OK, $resp = null) {
             http_response_code($code);
             header(ResponseType::JSON_TYPE);
-            $resp = json_encode(array("result"=>$resp, "mgs"=>$msg, "code"=>$code));
+            $resp = json_encode(array("result"=>$resp, "msg"=>$msg, "code"=>$code));
             echo($resp);
         }
     }
