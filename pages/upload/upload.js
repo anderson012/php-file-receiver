8f332797e60317ee6509fdd4fb978ee3254a3b4a
Anderson Carlos <andersoncarlos28@gmail.com>
2022-08-31 01:00:15 -0400

    feat: add support to splice blob files and upload one by one

--- pages/upload/upload.js
+++ pages/upload/upload.js
@@ -1,4 +1,4 @@
-import { generateVersion, makeLoading, validate, getAuth } from "../templates/utils.js"
+import { generateVersion, makeLoading, validate, getAuth, splitFile } from "../templates/utils.js"
 
 (() => {
     const submit = document.getElementById("submit");
@@ -7,8 +7,15 @@ import { generateVersion, makeLoading, validate, getAuth } from "../templates/ut
     const fileVersion = document.getElementById("file-version");
     const btnVersion = document.getElementById("button-version");
 
-    const loading = makeLoading(uploadProgress);
-    let file = null;
+    let loading = makeLoading(uploadProgress);
+    /**
+     * @type {File}
+     */
+    let originalFile = null;
+    /**
+     * @type {Blob[]}
+     */
+    let files = [];
 
     /**
      * 
@@ -19,7 +26,7 @@ import { generateVersion, makeLoading, validate, getAuth } from "../templates/ut
         const valid = validate({
             version: fileVersion.value,
             ...getAuth(),
-            file: file,
+            file: originalFile,
         });
 
         if (!valid) {
@@ -28,30 +35,12 @@ import { generateVersion, makeLoading, validate, getAuth } from "../templates/ut
         }
 
         try {
-            const form = new FormData();
-            form.append("file", file)
-            form.append("version", fileVersion.value);
-            submit.setAttribute("disabled", true);
-            fileInput.setAttribute("disabled", true);
-            loading.show();
-            loading.start();
-            const { data } = await axios.post("/routers/upload/index.php", form, {
-                headers: {
-                    "content-type": "multipart/form-data"
-                },
-                auth: getAuth(),
-                onUploadProgress: (event) => {
-                    let progress = (
-                        (event.loaded * 100) / event.total
-                    ).toFixed(2);
-        
-                    console.log(
-                        `A imagem ${file.name} está ${progress}% carregada... `
-                    )
-                    loading.setSize(progress, event.loaded);
-                },
-            });
-            swal.fire("Informação", `Arquivo enviado com sucesso! ${data.result ?? data.msg}`, "info");
+            let data = {};
+            for (let file of files) {
+                data = await uploadSingleFile(file, files.indexOf(file));
+            }
+            data = await joinFile(data.result);
+            swal.fire("Informação", `Arquivo enviado com sucesso! ${data.result ?? data.msg ?? ""}`, "info");
         } catch(e) {
             console.warn(e);
             swal.fire("Oops", e?.response?.data?.msg ?? e.message ?? "Processo inesperado no servidor", "warning");
@@ -63,14 +52,60 @@ import { generateVersion, makeLoading, validate, getAuth } from "../templates/ut
             loading.setSize(0);
         }
     }
+    /**
+     * @param {Blob} file 
+     * @returns 
+     */
+    async function uploadSingleFile(file, index) {
+        const form = new FormData();
+        form.append("file", file, `${originalFile.name}-part${index}`)
+        form.append("version", fileVersion.value);
+        submit.setAttribute("disabled", true);
+        fileInput.setAttribute("disabled", true);
+        loading = makeLoading(uploadProgress);
+        loading.show();
+        loading.start();
+        const {data} = await axios.post("/routers/upload/index.php", form, {
+            headers: {
+                "content-type": "multipart/form-data"
+            },
+            auth: getAuth(),
+            onUploadProgress: (event) => {
+                let progress = (
+                    (event.loaded * 100) / event.total
+                ).toFixed(2);
+
+                loading.setSize(progress, event.loaded);
+            },
+        });
+        return data;
+    }
+
+    async function joinFile(filePart) {
+        const form = new FormData();
+        form.append("targetFile", filePart.split("-part")[0])
+        form.append("chunks", files.length);
+        swal.fire("Juntanto arquivos");
+        swal.showLoading();
+        const {data} = await axios.post("/routers/join-parts/index.php", form, {
+            headers: {
+                "content-type": "multipart/form-data"
+            },
+            auth: getAuth(),
+        });
+
+        return data;
+    }
 
     /**
      * @param {HTMLInputElement} e
      */
     function onChangeFile(e){
-        file = e.target.files[0];
-        console.log(file);
-        if (file) {
+        originalFile = e.target.files[0];
+
+        if (originalFile) {
+            files = splitFile(originalFile);
+            console.log(files);
             submit.removeAttribute("disabled");
         }
     }
